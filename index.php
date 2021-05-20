e53fd3a3cea4d713d1d963e1d73893a4ae34a092
ildragonex88 <55831987+ildragonex88@users.noreply.github.com>
2021-05-20 12:12:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
-$__hostsdeny__ = array(); 
-$__content_type__ = 'image/gif';
-$__timeout__ = 0;
 $__content__ = '';
-$__password__ = base64_decode('MzQ1YQ==');
+$__content_type__ = 'application/zip';
+$__password__ = base64_decode("MzQ1YQ==");
 function message_html($title, $banner, $detail) {
-$error = "${banner} / ${detail}";
+$error = "<meta http-equiv='content-type' content='text/html;charset=utf-8'>
+<title>${title}</title>
+<H1>${banner}</H1></br>
+${detail}";
 return $error;
 }
 function decode_request($data) {
+global $__password__;
 list($headers_length) = array_values(unpack('n', substr($data, 0, 2)));
-$headers_data = gzinflate(substr($data, 2, $headers_length));
+$headers_data = substr($data, 2, $headers_length);
+$headers_data  = $headers_data ^ str_repeat($__password__, strlen($headers_data)); //
+$headers_data = gzinflate($headers_data);
 $body = substr($data, 2+intval($headers_length));
 $lines = explode("\r\n", $headers_data);
 $request_line_items = explode(" ", array_shift($lines));
@@ -34,6 +38,7 @@ $headers[$key] = $value;
 }
 if (isset($headers['Content-Encoding'])) {
 if ($headers['Content-Encoding'] == 'deflate') {
+$body  = $body ^ str_repeat($__password__, strlen($body));
 $body = gzinflate($body);
 $headers['Content-Length'] = strval(strlen($body));
 unset($headers['Content-Encoding']);
@@ -42,26 +47,24 @@ unset($headers['Content-Encoding']);
 return array($method, $url, $headers, $kwargs, $body);
 }
 function echo_content($content) {
-global $__password__, $__content_type__;
-if ($__content_type__ == 'image/gif') {
+global $__password__;
 echo $content ^ str_repeat($__password__[0], strlen($content));
-} else {
-echo $content;
-}
 }
 function curl_header_function($ch, $header) {
 global $__content__, $__content_type__;
 $pos = strpos($header, ':');
 if ($pos == false) {
 $__content__ .= $header;
-} else {
+} 
+else {
 $key = join('-', array_map('ucfirst', explode('-', substr($header, 0, $pos))));
 if ($key != 'Transfer-Encoding') {
 $__content__ .= $key . substr($header, $pos);
 }
 }
 if (preg_match('@^Content-Type: ?(audio/|image/|video/|application/octet-stream)@i', $header)) {
-$__content_type__ = 'image/x-png';
+//$__content_type__ = 'application/x-msdownload';
+$__content_type__ = 'application/vnd.microsoft.portable-executable';
 }
 if (!trim($header)) {
 header('Content-Type: ' . $__content_type__);
@@ -78,38 +81,20 @@ echo_content($content);
 return strlen($content);
 }
 function post() {
-list($method, $url, $headers, $kwargs, $body) = @decode_request(@file_get_contents('php://input'));
-$password = $GLOBALS['__password__'];
-if ($password) {
-if (!isset($kwargs['password']) || $password != $kwargs['password']) {
-header("HTTP/1.0 403 Forbidden");
-echo message_html('403 Forbidden', 'Wrong Password', "please edit");
-exit(-1);
-}
-}
-$hostsdeny = $GLOBALS['__hostsdeny__'];
-if ($hostsdeny) {
-$urlparts = parse_url($url);
-$host = $urlparts['host'];
-foreach ($hostsdeny as $pattern) {
-if (substr($host, strlen($host)-strlen($pattern)) == $pattern) {
-echo_content("HTTP/1.0 403\r\n\r\n" . message_html('403 Forbidden', "hostsdeny matched($host)",  $url));
-exit(-1);
-}
-}
-}
+list($method, $url, $headers, $kwargs, $body) = decode_request(file_get_contents('php://input'));
 if ($body) {
 $headers['Content-Length'] = strval(strlen($body));
 }
-if (isset($headers['Connection'])) {
-$headers['Connection'] = 'close';
-}
+//if (isset($headers['Connection'])) {
+//$headers['Connection'] = 'close';
+//}
 $header_array = array();
 foreach ($headers as $key => $value) {
 $header_array[] = join('-', array_map('ucfirst', explode('-', $key))).': '.$value;
 }
-$timeout = $GLOBALS['__timeout__'];
 $curl_opt = array();
+$ch = curl_init();
+$curl_opt[CURLOPT_URL] = $url;
 switch (strtoupper($method)) {
 case 'HEAD':
 $curl_opt[CURLOPT_NOBODY] = true;
@@ -120,15 +105,18 @@ case 'POST':
 $curl_opt[CURLOPT_POST] = true;
 $curl_opt[CURLOPT_POSTFIELDS] = $body;
 break;
-case 'PUT':
-case 'PATCH':
 case 'DELETE':
+case 'PATCH':
 $curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
 $curl_opt[CURLOPT_POSTFIELDS] = $body;
 break;
+case 'PUT':
+$curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
+$curl_opt[CURLOPT_POSTFIELDS] = $body;
+$curl_opt[CURLOPT_NOBODY] = true; 
+break;
 case 'OPTIONS':
 $curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
-$curl_opt[CURLOPT_NOBODY] = true;
 break;
 default:
 echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', 'Invalid Method: ' . $method,  $url));
@@ -140,42 +128,32 @@ $curl_opt[CURLOPT_BINARYTRANSFER] = true;
 $curl_opt[CURLOPT_HEADER] = false;
 $curl_opt[CURLOPT_HEADERFUNCTION] = 'curl_header_function';
 $curl_opt[CURLOPT_WRITEFUNCTION]  = 'curl_write_function';
+$curl_opt[CURLOPT_FAILONERROR] = false;
 $curl_opt[CURLOPT_FOLLOWLOCATION] = false;
-$curl_opt[CURLOPT_CONNECTTIMEOUT] = 10;
-$curl_opt[CURLOPT_TIMEOUT] = $timeout;
+$curl_opt[CURLOPT_TIMEOUT] = 30;
 $curl_opt[CURLOPT_SSL_VERIFYPEER] = false;
 $curl_opt[CURLOPT_SSL_VERIFYHOST] = false;
-$ch = curl_init($url);
+$curl_opt[CURLOPT_IPRESOLVE] = CURL_IPRESOLVE_V4;
 curl_setopt_array($ch, $curl_opt);
 curl_exec($ch);
-$errno = curl_errno($ch);
 if ($GLOBALS['__content__']) {
 echo_content($GLOBALS['__content__']);
-} else if ($errno) {
-if (!headers_sent()) {
-header('Content-Type: ' . $__content_type__);
-}
-$content = "HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', "PHP Urlfetch Error curl($errno)",  curl_error($ch));
-echo_content($content);
-}
+} 
 curl_close($ch);
 }
 function get() {
-echo "Ковертер файла jpeg to gif.
-      <form method='GET' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-	  <input type='pass' name='pass'>
-      <input type='submit' name='submit'' value='ok'>
-      </form>";
-     if(isset($_GET['submit'])) {
-	echo "error";
-}
+echo "Быстрый сжиматель 1 </br>
+<form enctype='multipart/form-data' action='indexx.php' method='GET'>
+<input type='hidden' name='MAX_FILE_SIZE' value='300000' />
+<input name='userfile' type='file' />
+ <label for='pwd'>Password:</label>
+<input type='password' id='pwd' name='pwd'> 
+<input type='submit' name='submit' value='Ћтправить файл' />
+</form>";
+exit;
 }
 function main() {
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-post();
-} else {
-get();
-}
-}
+post(); } else {
+get(); } }
 main();
