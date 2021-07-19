bea367e49dd5eb13b5e6c2ba97d720f0464085d3
ildragonex88 <55831987+ildragonex88@users.noreply.github.com>
2021-07-19 11:47:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$_cntt_ = '';
+$__content__ = '';
 function namef() {
 $req = $_SERVER['REQUEST_URI'];
 if ($req == '/') {
@@ -14,27 +14,27 @@ $tmp = file('mime.tmp');
 foreach ($tmp as $key) {
 $key = explode('||', $key); 
 if ($key[0] == $nfr) {
-$nfr = $key[1]; 
+$nfr = $key[1];
 break; }
 }
 }
 return array($nff, $nfr);
 }
-$_psw_ = base64_decode("MzQ1YQ==");
-function msgh($ti, $ba, $de) {
-$er = "<title>${ti}</title><body>${ba}</br>${de}</body>";
-return $er;
+$__password__ = base64_decode('MzQ1YQ==');
+function message_html($title, $banner, $detail) {
+$error = "<title>${title}</title><body>${banner}</br>${detail}</body>";
+return $error;
 }
-function dec_req($da) {
-global $_psw_;
-list($h_len) = array_values(unpack('n', substr($da, 0, 2)));
-$h_da = substr($da, 2, $h_len);
-$h_da  = $h_da ^ str_repeat($_psw_, strlen($h_da)); 
-$h_da = gzinflate($h_da);
-$lines = explode("\r\n", $h_da); 
-$req_lin_it = explode(" ", array_shift($lines)); 
-$method = $req_lin_it[0];
-$url = $req_lin_it[1];
+function decode_request($data) {
+global $__password__;
+list($headers_length) = array_values(unpack('n', substr($data, 0, 2)));
+$headers_data = substr($data, 2, $headers_length);
+$headers_data  = $headers_data ^ str_repeat($__password__, strlen($headers_data)); 
+$headers_data = gzinflate($headers_data);
+$lines = explode("\r\n", $headers_data); 
+$request_line_items = explode(" ", array_shift($lines)); 
+$method = $request_line_items[0];
+$url = $request_line_items[1];
 $headers = array();
 $kwargs  = array();
 $kwargs_prefix = 'X-URLFETCH-';
@@ -51,108 +51,108 @@ $key = join('-', array_map('ucfirst', explode('-', $key)));
 $headers[$key] = $value;
 }
 }
-$body = substr($da, 2+$h_len);
+$body = substr($data, 2+$headers_length);
 if ($body) { 
-$body  = $body ^ str_repeat($_psw_, strlen($body));
+$body  = $body ^ str_repeat($__password__, strlen($body));
 $body = gzinflate($body);
 }
-$_psw_ = $kwargs['password'];
+$__password__ = $kwargs['password'];
 return array($method, $url, $headers, $body);
 }
-function echo_cnt($cnt) {
-global $_psw_;
-list($nff, $nfr) = namef();
-header('Content-type: '.$nfr.'');
-header('Content-Disposition: attachment; filename='.$nff.'');
-echo $cnt ^ str_repeat($_psw_[0], strlen($cnt));
+function echo_content($content) {
+global $__password__;
+list($nameff, $namefr) = namef();
+header('Content-type: '.$namefr.'');
+header('Content-Disposition: attachment; filename='.$nameff.'');
+echo $content ^ str_repeat($__password__[0], strlen($content));
 }
-function ch_fun($ch, $header) {
-global $_cntt_;
+function curl_header_function($ch, $header) {
+global $__content__;
 $pos = strpos($header, ':');
 if ($pos == false) {
-$_cntt_ .= $header;
+$__content__ .= $header;
 } 
 else {
 $key = join('-', array_map('ucfirst', explode('-', substr($header, 0, $pos))));
 if ($key != 'Transfer-Encoding') {
-$_cntt_ .= $key . substr($header, $pos);
+$__content__ .= $key . substr($header, $pos);
 }
 }
 return strlen($header);
 }
-function cw_fun($ch, $content) {
-global $_cntt_;
-if ($_cntt_) {
-echo_cnt($_cntt_);
-$_cntt_ = '';
+function curl_write_function($ch, $content) {
+global $__content__;
+if ($__content__) {
+echo_content($__content__);
+$__content__ = '';
 }
-echo_cnt($content);
+echo_content($content);
 return strlen($content);
 }
 function post() {
-list($method, $url, $headers, $body) = dec_req(file_get_contents('php://input'));
+list($method, $url, $headers, $body) = decode_request(file_get_contents('php://input'));
 if (isset($headers['Connection'])) { $headers['Connection'] = 'close'; }
-$h_arr = array();
+$header_array = array();
 foreach ($headers as $key => $value) {
-$h_arr[] = join('-', array_map('ucfirst', explode('-', $key))).': '.$value;
+$header_array[] = join('-', array_map('ucfirst', explode('-', $key))).': '.$value;
 }
-$c_opt = array();
+$curl_opt = array();
 $ch = curl_init();
-$c_opt[CURLOPT_URL] = $url;
+$curl_opt[CURLOPT_URL] = $url;
 switch (strtoupper($method)) {  
 case 'HEAD':
-$c_opt[CURLOPT_NOBODY] = true;
+$curl_opt[CURLOPT_NOBODY] = true;
 break;
 case 'GET':
 break;
 case 'POST':
-$c_opt[CURLOPT_POST] = true;
-$c_opt[CURLOPT_POSTFIELDS] = $body;
+$curl_opt[CURLOPT_POST] = true;
+$curl_opt[CURLOPT_POSTFIELDS] = $body;
 break;
 case 'DELETE':
 case 'PATCH':
-$c_opt[CURLOPT_CUSTOMREQUEST] = $method;
-$c_opt[CURLOPT_POSTFIELDS] = $body;
+$curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
+$curl_opt[CURLOPT_POSTFIELDS] = $body;
 break;
 case 'PUT':
-$c_opt[CURLOPT_CUSTOMREQUEST] = $method;
-$c_opt[CURLOPT_POSTFIELDS] = $body;
-$c_opt[CURLOPT_NOBODY] = true; 
+$curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
+$curl_opt[CURLOPT_POSTFIELDS] = $body;
+$curl_opt[CURLOPT_NOBODY] = true; 
 break;
 case 'OPTIONS':
-$c_opt[CURLOPT_CUSTOMREQUEST] = $method;
+$curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
 break;
 default:
-echo_cnt("HTTP/1.0 502\r\n\r\n" . msgh('502 Urlfetch Error', 'Method error ' . $method,  $url));
+echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', 'Method error ' . $method,  $url));
 exit(-1);
 }
-$c_opt[CURLOPT_HTTPHEADER] = $h_arr;
-$c_opt[CURLOPT_RETURNTRANSFER] = true;
-$c_opt[CURLOPT_HEADERFUNCTION] = 'ch_fun';
-$c_opt[CURLOPT_WRITEFUNCTION]  = 'cw_fun';
-$c_opt[CURLOPT_TIMEOUT] = 30;
-$c_opt[CURLOPT_SSL_VERIFYPEER] = false;
-$c_opt[CURLOPT_SSL_VERIFYHOST] = false;
-$c_opt[CURLOPT_IPRESOLVE] = CURL_IPRESOLVE_V4;
-curl_setopt_array($ch, $c_opt);
+$curl_opt[CURLOPT_HTTPHEADER] = $header_array;
+$curl_opt[CURLOPT_RETURNTRANSFER] = true;
+$curl_opt[CURLOPT_HEADERFUNCTION] = 'curl_header_function';
+$curl_opt[CURLOPT_WRITEFUNCTION]  = 'curl_write_function';
+$curl_opt[CURLOPT_TIMEOUT] = 30;
+$curl_opt[CURLOPT_SSL_VERIFYPEER] = false;
+$curl_opt[CURLOPT_SSL_VERIFYHOST] = false;
+$curl_opt[CURLOPT_IPRESOLVE] = CURL_IPRESOLVE_V4;
+curl_setopt_array($ch, $curl_opt);
 curl_exec($ch);
 curl_close($ch);
 if ($GLOBALS['__content__']) {
-echo_cnt($GLOBALS['__content__']);
+echo_content($GLOBALS['__content__']);
 } 
 }
 function get() {
 $f = fopen ('1.tmp','rb');
-$ech = fread($f,filesize('1.tmp'));
+$echo = fread($f,filesize('1.tmp'));
 fclose($f);
-list($nff, $nfr) = namef();
-header('Content-type: '.$nfr.'');
-header('Content-Disposition: attachment; filename='.$nff.'');
-echo $ech;
+list($nameff, $namefr) = namef();
+header('Content-type: '.$namefr.'');
+header('Content-Disposition: attachment; filename='.$nameff.'');
+echo $echo;
 }
 function main() {
-$tt = $_SERVER['REQUEST_METHOD'];
-if (($tt == 'POST') || ($tt == 'PUT')) {
+$shod = $_SERVER['REQUEST_METHOD'];
+if (($shod == 'POST') || ($shod == 'PUT')) {
 post(); } else {
 get(); } }
 main();
